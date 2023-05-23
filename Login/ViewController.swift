3f7e80dff9cdc6ee9c607fd2c44b978adda36d16
Ednan <52186220+ednanf@users.noreply.github.com>
2023-05-23 12:58:53 -0300

    Project completed.

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -9,15 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+// MARK: - IBOutlets
     @IBOutlet var usernameField: UITextField!
-    
+    @IBOutlet var forgotUsernameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+
+// MARK: - Setup
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+// MARK: - Segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = usernameField.text
+        guard let sender = sender as? UIButton else { return }                          // First we unwrap the sender parameter (which is an optional).
+                                                                                        // We also cast it as UIButton (which in this case always succeeds).
+        if sender == forgotPasswordButton {
+            segue.destination.navigationItem.title = "Forgot Password"                  // The ForgotUsernameOrPassword segue is triggered by the performSegue,
+        } else if sender == forgotUsernameButton {                                      // which we passed in the corresponding button as a sender.
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title = usernameField.text                 // The login segue is specifically triggered by a button.
+        }
+        
     }
+    
+// MARK: - IBAction
+    @IBAction func forgotUsernameButton(_ sender: Any) {
+        performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: sender)     // The sender is the button that was tapped.
+    }
+    
+    @IBAction func forgorPasswordButton(_ sender: Any) {
+        performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: sender)
+    }
+    
 }
 
